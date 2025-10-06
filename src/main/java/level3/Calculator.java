4796c8691f9b83fd876237c1d253b8b8863c483f
Albert Medina <albertmecu@gmail.com>
2025-10-06 23:10:44 +0200

    feat: using delta to work with double in the calculator

--- src/main/java/level3/Calculator.java
+++ src/main/java/level3/Calculator.java
@@ -5,19 +5,19 @@ public class Calculator {
     public Calculator() {
     }
 
-    public static float sum(float a, float b) {
+    public static double sum(double a, double b) {
         return a + b;
     }
 
-    public static float substraction(float a, float b) {
+    public static double substraction(double a, double b) {
         return a - b;
     }
 
-    public static float multiplication(float a, float b) {
+    public static double multiplication(double a, double b) {
         return a * b;
     }
 
-    public static float division(float a, float b) {
+    public static double division(double a, double b) {
         return a / b;
     }
 }
--- src/test/java/level3/CalculatorTest.java
+++ src/test/java/level3/CalculatorTest.java
@@ -9,21 +9,21 @@ public class CalculatorTest {
 
     @Test
     void checkSum() {
-        assertEquals(3.7f, Calculator.sum(2.5f, 1.2f));
+        assertEquals(3.7f, Calculator.sum(2.5, 1.2), 0.0001);
     }
 
     @Test
     void checkSubstraction() {
-        assertEquals(5.8f, Calculator.substraction(12.2f, 6.4f));
+        assertEquals(5.8, Calculator.substraction(12.2, 6.4), 0.0001);
     }
 
     @Test
     void checkMultiplication() {
-        assertEquals(7.7f, Calculator.multiplication(3.5f, 2.2f));
+        assertEquals(7.7, Calculator.multiplication(3.5, 2.2), 0.0001);
     }
 
     @Test
     void checkDivision() {
-        assertEquals(4.5f, Calculator.division(9.0f, 2.0f));
+        assertEquals(4.5, Calculator.division(9.0, 2.0), 0.0001);
     }
 }
