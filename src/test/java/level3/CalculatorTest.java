6d0923210f4a99edac8be350d1464b3d9719139c
Albert Medina <albertmecu@gmail.com>
2025-10-06 23:08:47 +0200

    feat: first approach of the calculator implementation

--- src/test/java/level3/CalculatorTest.java
+++ src/test/java/level3/CalculatorTest.java
@@ -9,21 +9,21 @@ public class CalculatorTest {
 
     @Test
     void checkSum() {
-        assertEquals(3.7, Calculator.sum(2.5, 1.2));
+        assertEquals(3.7f, Calculator.sum(2.5f, 1.2f));
     }
 
     @Test
     void checkSubstraction() {
-        assertEquals(5.8, Calculator.substraction(12.2, 6.4));
+        assertEquals(5.8f, Calculator.substraction(12.2f, 6.4f));
     }
 
     @Test
     void checkMultiplication() {
-        assertEquals(7.7, Calculator.multiplication(3.5, 2.2));
+        assertEquals(7.7f, Calculator.multiplication(3.5f, 2.2f));
     }
 
     @Test
     void checkDivision() {
-        assertEquals(4.5, Calculator.division(9.0, 2.0));
+        assertEquals(4.5f, Calculator.division(9.0f, 2.0f));
     }
 }
