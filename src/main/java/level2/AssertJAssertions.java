d7f0f0b7b0ea69a4d57b1b959b03cb9b16466315
Albert Medina <albertmecu@gmail.com>
2025-10-06 22:31:35 +0200

    feat: implemented all required tests using AssertJ assertions

---
+++ src/main/java/level2/AssertJAssertions.java
@@ -0,0 +1,12 @@
+package level2;
+
+public class AssertJAssertions {
+
+    public AssertJAssertions() {
+    }
+
+    public static String getNumberName(int n) {
+        String[] arr = {"Zero", "One", "Two", "Three"};
+        return arr[n];
+    }
+}
