7542a7133bd9cb041fdc48388eb7b87606bd4d86
christo256 <ckingmaster2@gmail.com>
2025-10-16 11:45:51 +0200

    fix: removed empty constructors (close #6)

--- src/main/java/level1ex3/ArrayIndexOutOfBounds.java
+++ src/main/java/level1ex3/ArrayIndexOutOfBounds.java
@@ -2,9 +2,6 @@ package level1ex3;
 
 public class ArrayIndexOutOfBounds {
 
-    public ArrayIndexOutOfBounds() {
-    }
-
     public static void triggerArrayIndexOutOfBounds() {
         int[] arr = {1, 2, 3, 4, 5};
         int num = arr[5];
